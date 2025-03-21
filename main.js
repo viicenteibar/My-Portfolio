06d1dfaf9b5c3378da81b48030e4ec819136b6ce
Vicente Ibarzabal <vicenteibarzabal@gmail.com>
2025-03-21 14:03:07 -0300

    cambio de diseño

--- main.js
+++ main.js
@@ -14,18 +14,3 @@ function handleMouseMove( event ) {
         element.style.flexBasis = (window.innerWidth - event.clientX) + 'px';
     } );
 }
-
-const img = document.getElementById("correoImg");
-
-        img.addEventListener("click", () => {
-            img.classList.add("animate__animated", "animate__tada");
-            img.style.transition = "2s cubic-bezier(.77,.28,.42,.93)";
-            img.style.transform = "scale(1.05)";
-            img.src = "./img/correo-verde-claro.png"; // Cambia a la imagen verde
-
-            img.addEventListener("animationend", () => {
-                img.classList.remove("animate__tada");
-                img.style.transform = "scale(1)"; // Vuelve al tamaño original
-                img.src = "./img/correo.png"; // Vuelve a la imagen original
-            }, { once: true });
-        });
